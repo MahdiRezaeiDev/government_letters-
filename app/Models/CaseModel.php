a5eb17dcf1a7d313520387336c7523d466baaaa8
Mahdi Rezaei <mahdi.sohaily4030@gmail.com>
2026-04-09 12:19:05 +0430

    Added the model files

---
+++ app/Models/CaseModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Table;
+use Illuminate\Database\Eloquent\Model;
+class CaseModel extends Model
+{
+    protected $table = "cases";
+}
